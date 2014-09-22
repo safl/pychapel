234f75122f47903b4cac16471e04d0955246699f
Simon A. F. Lund <safl@safl.dk>
2014-09-22 10:57:04 -0700

    Aha, much better Extern representation.

--- module/examples/hello.library.py
+++ module/examples/hello.library.py
@@ -1,39 +1,30 @@
-from pych.extern import FromC
+from pych.extern import Extern
 
 #
-# Mapping the Python function "hello_cworld" to the
-# C-function "hello_c".
+# Mapping the Python function "hello_world" to the function "hello_world"
+# from the library "libexamples.so".
 #
-@FromC(elib="libexamples.so", ename="hello_world")
-def hello_cworld():
+@Extern(elib="libexamples.so", ename="hello_world")
+def hello_world():
     return None
 
 #
-# Mapping the Python function "" to the
-# C-function "hello_c".
+# Mapping the Python function "add_ints" to the function "add_ints"
+# from the library "libexamples.so".
 #
-@FromC(elib="libexamples.so", ename="add_ints")
+@Extern(elib="libexamples.so", ename="add_ints")
 def add_ints(x=int, y=int):
     return int
 
 #
-# Mapping the Python function "" to the
-# C-function "hello_c".
+# Mapping the Python function "add_doubles" to the function "add_doubles"
+# from the library "libexamples.so".
 #
-@FromC(elib="libexamples.so", ename="add_doubles")
+@Extern(elib="libexamples.so", ename="add_doubles")
 def add_doubles(x=float, y=float):
     return float
 
-#@FromC()
-#def add(x, y):
-#    """
-#    return x+y;
-#    """
-#    return int
-
 if __name__ == "__main__":
-    print hello_cworld()
-    #print add_ints(1, 2)
-    #print add_doubles(1.0, 2.0)
-    #print add_doubles(1.0, 2.0)
-    #print add_doubles(1.0, 2.0)
+    print hello_world()
+    print add_ints(1, 2)
+    print add_doubles(1.0, 2.0)
--- module/pych/extern.py
+++ module/pych/extern.py
@@ -16,68 +16,67 @@ typemap = {
     unicode:    ctypes.c_wchar_p
 }
 
-class Extern(object):
-    """
-    Encapsulates a mapping between a Python function and an external function.
-    """
-
-    def __init__(self, pfunc=None, pname=None, doc=None,
-                 atypes=[], anames=[], rtype=None,
-                 efunc=None, ename=None, elib=None,
-                 sfile=None, slang=None):
-
-        self.pfunc  = pfunc     # Python function handle
-        self.pname  = pname     # Python function name
-        self.doc    = doc       # Python doc-string (used for inline source-code)
-        self.atypes = atypes    # Python types of function arguments
-        self.anames = anames    # Python names of function arguments
-        self.rtype  = rtype     # Python type for the return value
-
-        self.efunc  = efunc     # ctypes function handle
-        self.ename  = ename     # External function name
-        self.elib   = elib      # Library filename
-        self.sfile  = sfile     # File with sourcecode
-        self.slang  = slang     # Language of the 
-
-    def __repr__(self):
-        return pprint.pformat(vars(self))
+#class Extern(object):
+#    """
+#    Encapsulates a mapping between a Python function and an external function.
+#    """
+#
+#    def __init__(self, pfunc=None, pname=None, doc=None,
+#                 atypes=[], anames=[], rtype=None,
+#                 efunc=None, ename=None, elib=None,
+#                 sfile=None, slang=None):
+#
+#        self.pfunc  = pfunc     # Python function handle
+#        self.pname  = pname     # Python function name
+#        self.doc    = doc       # Python doc-string (used for inline source-code)
+#        self.atypes = atypes    # Python types of function arguments
+#        self.anames = anames    # Python names of function arguments
+#        self.rtype  = rtype     # Python type for the return value
+#
+#        self.efunc  = efunc     # ctypes function handle
+#        self.ename  = ename     # External function name
+#        self.elib   = elib      # Library filename
+#        self.sfile  = sfile     # File with sourcecode
+#        self.slang  = slang     # Language of the 
+#
+#    def __repr__(self):
+#        return pprint.pformat(vars(self))
 
 #
 # Decorate a Python-function with these to construct a mapping to an Extern.
 #
-class FromExtern(object):
-    """Encapsulates a call to an external function."""
+class Extern(object):
+    """Encapsulates a mapping and call to an external function."""
 
     def __init__(self, ename=None, elib=None, sfile=None, slang=None):
 
         # This is done only once; when the function is decorated
-        self._extern = Extern(
-            pfunc   = None,
-            pname   = None,
-            doc     = None,
-            atypes  = [],
-            anames  = [],
-            rtype   = None,
+        self.pfunc   = None
+        self.pname   = None
+        self.doc     = None
+        self.atypes  = []
+        self.anames  = []
+        self.rtype   = None
 
-            efunc   = None,
-            ename   = ename,
-            elib    = elib,
+        self.efunc   = None
+        self.ename   = ename
+        self.elib    = elib
 
-            sfile   = sfile,
-            slang   = slang
-        )
+        self.sfile   = sfile
+        self.slang   = slang
+
         logging.debug("__init__decorate__")
 
     def _validate_decl(self):
         """Validate the function declaration."""
 
-        if len(self._extern.atypes) != len(self._extern.anames):
+        if len(self.atypes) != len(self.anames):
             # Check that we have sufficient amount of type-declarations
             raise TypeError("Missing type declaration on arguments.")
         else:
             # Check that declared types are supported
-            for i, arg in enumerate(self._extern.anames):
-                arg_type = self._extern.atypes[i]
+            for i, arg in enumerate(self.anames):
+                arg_type = self.atypes[i]
                 if arg_type not in typemap:
                     msg = "Unsupported type: %s for argument %s" % (
                         arg_type, arg
@@ -88,46 +87,46 @@ class FromExtern(object):
         """Compare argument-types with extern declation."""
 
         call_types = [type(arg) for arg in args]
-        if call_types != self._extern.atypes:
+        if call_types != self.atypes:
             ct_text = pprint.pformat(call_types)
-            at_text = pprint.pformat(self._extern.atypes)
+            at_text = pprint.pformat(self.atypes)
             raise TypeError("Unsupported arg-types; %s. Expected: %s" %
                             (ct_text, at_text))
 
     def __call__(self, f):
         # This is done only once; when the function is decorated
 
-        self._extern.pfunc  = f
-        self._extern.pname  = f.__name__
-        self._extern.doc    = f.__doc__
+        self.pfunc  = f
+        self.pname  = f.__name__
+        self.doc    = f.__doc__
 
         #
         # Infer type-declaration of Extern from the crazy convention
-        arg_spec = inspect.getargspec(self._extern.pfunc)
+        arg_spec = inspect.getargspec(self.pfunc)
 
-        self._extern.anames = arg_spec.args         # Extract argument names
+        self.anames = arg_spec.args         # Extract argument names
         if arg_spec.defaults:                       # Extract argument types
-            self._extern.atypes = list(arg_spec.defaults)
-        self._extern.rtype  = self._extern.pfunc()  # Obtain return-type
+            self.atypes = list(arg_spec.defaults)
+        self.rtype  = self.pfunc()  # Obtain return-type
 
         self._validate_decl()                       # Validate declaration
 
         # 
         # Extract attributes for "inline" function
-        if self._extern.doc:
-            self._extern.ename = self._extern.pname
-            self._extern.elib  = "inline.so"
+        if self.doc:
+            self.ename = self.pname
+            self.elib  = "inline.so"
 
         #
         # Extract attributes for "sfile" function
-        if self._extern.sfile:
+        if self.sfile:
             # TODO: Consider parsing the source-file and expanding validation
             #       of the type-declaration using the parsed information.
-            if not self._extern.ename:
-                self._extern.ename = self._extern.pname
+            if not self.ename:
+                self.ename = self.pname
 
-            self._extern.elib = "lib%s.so" % os.path.splitext(os.path.basename(
-                self._extern.sfile
+            self.elib = "lib%s.so" % os.path.splitext(os.path.basename(
+                self.sfile
             ))[0]
 
         #
@@ -137,7 +136,7 @@ class FromExtern(object):
         # function ahead of time. Or compile all hinted functions
         # in one go as the first call hits a function.
         #
-        pych.runtime.instance.hint(self._extern)
+        pych.runtime.instance.hint(self)
 
         def wrapped_f(*args):
             # This is invoked on each function-call
@@ -147,27 +146,27 @@ class FromExtern(object):
 
             #
             # Obtain the external object.
-            if not self._extern.efunc:
-                efunc = pych.runtime.instance.materialize(self._extern)
+            if not self.efunc:
+                efunc = pych.runtime.instance.materialize(self)
 
                 if not efunc:
                     raise Exception("Failed materializing function!")
                 #
                 # Register argument conversion functions on efunc
-                efunc.argtypes = [typemap[atype] for atype in self._extern.atypes]
-                efunc.restype  = typemap[self._extern.rtype]
+                efunc.argtypes = [typemap[atype] for atype in self.atypes]
+                efunc.restype  = typemap[self.rtype]
 
                 #
                 # Register the efunc handle on Extern
-                self._extern.efunc = efunc
+                self.efunc = efunc
 
             #
             # Call
-            return self._extern.efunc(*args)
+            return self.efunc(*args)
 
         return wrapped_f
 
-class FromC(FromExtern):
+class FromC(Extern):
 
     def __init__(self, ename=None, elib=None, sfile=None):
         super(FromC, self).__init__(
@@ -178,7 +177,7 @@ class FromC(FromExtern):
             slang = "C"
         )
 
-class FromChapel(FromExtern):
+class FromChapel(Extern):
 
     def __init__(self, ename=None, elib=None, sfile=None):
         super(FromChapel, self).__init__(
