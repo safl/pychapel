a3e56bd4d0bfc44959c78005deb969285b861d49
Simon A. F. Lund <safl@safl.dk>
2014-11-12 13:02:22 -0800

    ...

--- module/setup.py
+++ module/setup.py
@@ -38,6 +38,8 @@ setup(
         ('share/pych/examples', glob.glob('../docs/source/examples/*')),
         ('share/pych/include', glob.glob('include/*.h')),
         ('share/pych/lib', glob.glob('lib/*')),
+        ('share/pych/sfiles/c', glob.glob('sfiles/c/*')),
+        ('share/pych/sfiles/chapel', glob.glob('sfiles/chapel/*')),
         ('share/pych/templates/c', glob.glob('templates/c/*')),
         ('share/pych/templates/chapel', glob.glob('templates/chapel/*')),
         ('share/pych/templates/python', glob.glob('templates/python/*')),
