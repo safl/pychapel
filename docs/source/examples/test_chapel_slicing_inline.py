bc1d553f47febe173a41a6d506c4940bfe9193ab
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2015-01-07 11:02:42 -0800

    Clean up test_chapel_slicing_inline.py
    For whatever reason, when I was first testing this, having multiple asserts or
    asserts in certain places in this test caused py.test to make
    test_pych_reduce_add.py fail.  However, after switching git branches, I could
    make these changes (which aren't different from before!) and py.test did not
    complain.  Computers are weird.

--- docs/source/examples/test_chapel_slicing_inline.py
+++ docs/source/examples/test_chapel_slicing_inline.py
@@ -19,16 +19,22 @@ def ex_numpy(a=np.ndarray):
 def test_chapel_slicing():
     a = np.ones((10), dtype=np.float)
     b = a[::2]
-    wasRight1 = all(a == [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ])
-    # wasRight1 ensures array originally started as filled with ones
+    assert all(a == [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ])
+    # this assert ensures array originally started as filled with ones
     ex_numpy(b)
-    wasRight2 = all(a[::2] == b)
-    # wasRight2 ensures a was updated as b was, since slices of numpy arrays
+    assert all(a[::2] == b)
+    # this assert ensures a was updated when b was, since slices of numpy arrays
     # share memory with the original array
+    # b should contain references to the current time, but since that will vary
+    # from run to run, this is a more robust comparison
 
     c = np.ones((10), dtype=np.float)
+    # c starts out the same as a
     d = c[::2]
     for i, e in enumerate(d):
         d[i] = i
+    # d should contain 0.0 through 4.0, updating alternating indexes in c
+    # starting with the first index to contain the same contents.  The
+    # assert in the following line reflects this knowledge.
 
-    assert wasRight1 and wasRight2 and all(c == [ 0.0,  1.0,  1.0,  1.0,  2.0,  1.0,  3.0,  1.0,  4.0,  1.0]) and all(d == [ 0.0,  1.0,  2.0,  3.0,  4.0])
+    assert all(c == [ 0.0,  1.0,  1.0,  1.0,  2.0,  1.0,  3.0,  1.0,  4.0,  1.0]) and all(d == [ 0.0,  1.0,  2.0,  3.0,  4.0])
