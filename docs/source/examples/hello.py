b1d913f82eca7a27c2236594083872030320e52b
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2015-01-14 15:44:36 -0800

    The comments in hello.py were out of date with the function headers.
    This example does not run as is, though, and whether that will change in the
    future is unclear.

--- docs/source/examples/hello.py
+++ docs/source/examples/hello.py
@@ -5,23 +5,23 @@ from pych.extern import FromC
 
 #
 # Mapping the Python function "hello_cworld" to the
-# C-function "hello_c".
+# C-function "hello_world".
 #
 @FromC(clib="libexamples.so", ename="hello_world")
 def hello_cworld():
     return None
 
 #
-# Mapping the Python function "" to the
-# C-function "hello_c".
+# Mapping the Python function "add_ints" to the
+# C-function "add_ints".
 #
 @FromC(clib="libexamples.so", ename="add_ints")
 def add_ints(x=int, y=int):
     return int
 
 #
-# Mapping the Python function "" to the
-# C-function "hello_c".
+# Mapping the Python function "add_doubles" to the
+# C-function "add_doubles".
 #
 @FromC(clib="libexamples.so", ename="add_doubles")
 def add_doubles(x=float, y=float):
