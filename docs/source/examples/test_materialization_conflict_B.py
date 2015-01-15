797c572a35c0bf233ef9a5cb0b23e87d17c595e5
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2015-01-14 16:17:20 -0800

    Incorrect(?) Materialization Error example
    These two tests demonstrate an error that pytest had exercised at one point,
    which is: if two extern functions are named the same, the first one used will
    execute for all attempts to access an extern function by that name (even if the
    intended source file of the other differs).  For instance, if I have a function
    foo in module1 and a function foo in module2, if module2.foo is different from
    module1.foo but module1.foo is called first, when I try to call module2.foo,
    the result will be the same as if I had called module1.foo again.
    
    I think this is a bug and that the materialization of functions should take into
    account their sources (i.e. inline vs. named bfile vs. named sfile) and if the
    source differs from the original materialization, a second materialization
    should be made.
    
    If we have support for accessing multiple modules in a Chapel file, it'd be nice
    for the separate materialization to apply to functions in separate modules.
    That will probably be tricky, though.

---
+++ docs/source/examples/test_materialization_conflict_B.py
@@ -0,0 +1,33 @@
+from pych.extern import Chapel
+
+@Chapel(bfile="material.file2.chpl")
+def material():
+    return None
+
+if __name__ == '__main__':
+    material()
+    import test_materialization_conflict_A
+    test_materialization_conflict_A.material()
+
+import testcase
+# contains the general testing method, which allows us to gather output
+import os.path
+
+def test_materialization_A():
+    out = testcase.runpy(os.path.realpath(__file__))
+    # The first time this test is run, it may contain output notifying that
+    # a temporary file has been created.  The important part is that this
+    # expected output follows it (enabling the test to work for all runs, as
+    # the temporary file message won't occur in the second run)  But that means
+    # we can't use out.startswith
+    print out
+
+    # ensure contains both outputs
+    myLoc = out.find("I'm file 2!\n")
+    assert myLoc >= 0
+    otherLoc = out.find("I'm file 1!\n")
+    assert otherLoc >= 0
+    # I expect this assert to fail.
+
+    # ensure my output is first
+    assert otherLoc > myLoc
